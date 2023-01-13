4f37588343c41ff8261438b30f3a7b390adf01c8
Fellipe Neves <fellipe2.0e@gmail.com>
2023-01-12 22:35:42 -0300

    Learning about Components e Properties

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { Post } from './Post'
 
-function App() {
+export function App() {
   return (
-    <h1>Hello Ignite!</h1>
+    <div>
+      <Post 
+        author="Fellipe"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Error necessitatibus est fugit, et voluptatum qui aspernatur dignissimos accusamus eligendi nemo consequatur cupiditate nisi quisquam debitis veniam odio ipsam voluptates mollitia."
+      />
+      
+       <Post 
+        author="Joao"
+        content="Post Joao"
+      />
+      
+    </div>
   )
 }
 
-export default App
