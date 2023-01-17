96384a7bd93d79cc2414e0130236df8f26e34ee7
Fellipe Neves <fellipe2.0e@gmail.com>
2023-01-16 21:15:01 -0300

    Created header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
+import { Header } from './components/Header';
 import { Post } from './Post'
+import './global.css';
+
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post 
         author="Fellipe"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Error necessitatibus est fugit, et voluptatum qui aspernatur dignissimos accusamus eligendi nemo consequatur cupiditate nisi quisquam debitis veniam odio ipsam voluptates mollitia."
