858a8c029f6a3373ba27136d67d6e2a6dd981b8d
Fellipe Neves <fellipe2.0e@gmail.com>
2023-01-16 22:01:35 -0300

    Created Sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,30 @@
-import { useState } from 'react'
+
 import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
+
 import { Post } from './Post'
 import './global.css';
+import styles from './App.module.css';
 
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post 
-        author="Fellipe"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Error necessitatibus est fugit, et voluptatum qui aspernatur dignissimos accusamus eligendi nemo consequatur cupiditate nisi quisquam debitis veniam odio ipsam voluptates mollitia."
-      />
-      
-       <Post 
-        author="Joao"
-        content="Post Joao"
-      />
-      
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Fellipe"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Error necessitatibus est fugit, et voluptatum qui aspernatur dignissimos accusamus eligendi nemo consequatur cupiditate nisi quisquam debitis veniam odio ipsam voluptates mollitia."
+          />
+          
+          <Post 
+            author="Joao"
+            content="Post Joao"
+          />
+        </main>
+      </div>
     </div>
   )
 }
